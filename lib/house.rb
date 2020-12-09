4a5bc37882d666d34d5be87eb73621b48af17c31
Ben Fulton <benjamin.fulton99@gmail.com>
2020-12-09 13:33:41 -0700

    Add categorization and area methods

--- lib/house.rb
+++ lib/house.rb
@@ -12,4 +12,22 @@ class House
   def add_room(room)
     @rooms << room
   end
+
+  def above_market_average?
+    @price > 500000
+  end
+
+  def rooms_from_category(arg_cat)
+    @rooms.find_all do |room|
+      room.category == arg_cat
+    end
+  end
+
+  def area
+    total_area = 0
+    @rooms.each do |room|
+      total_area += room.area
+    end
+    total_area
+  end
 end
